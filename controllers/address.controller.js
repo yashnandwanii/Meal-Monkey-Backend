32132c29bbeb03089b4b48cdbfb93e15093fb5a5
yashnandwanii <yashnandwani47@gmail.com>
2025-06-12 00:13:44 +0530

    Improve error handling in address controller and streamline response structure for address retrieval

--- controllers/address.controller.js
+++ controllers/address.controller.js
@@ -18,14 +18,15 @@ const addAddress = async (req, res) => {
         newAddress.save();
         return res.status(201).json({ status: true, message: "Address added successfully", address: newAddress });
     } catch (error) {
-        
+         console.error("Error adding address:", error);
+        return res.status(500).json({ status: false, message: "Internal server error" });
     }
 }
 
 const getAddresses = async (req, res) => {
     try {
         const addresses = await Address.find({ userId: req.user.id });
-        return res.status(200).json({ status: true, addresses });
+        return res.status(200).json(addresses);
     } catch (error) {
         console.error("Error fetching addresses:", error);
         return res.status(500).json({ status: false, message: "Internal server error" });
@@ -79,7 +80,7 @@ const getDefaultAddress = async (req, res) => {
         if (!defaultAddress) {
             return res.status(404).json({ status: false, message: "Default address not found" });
         }
-        return res.status(200).json({ status: true, address: defaultAddress });
+        return res.status(200).json(defaultAddress );
     } catch (error) {
         console.error("Error fetching default address:", error);
         return res.status(500).json({ status: false, message: "Internal server error" });
